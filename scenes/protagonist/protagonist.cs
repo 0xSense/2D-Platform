b7bf8711055999e6d59fade25b18dba67ca04d07
Sense <105939929+0xSense@users.noreply.github.com>
2023-10-30 22:12:11 -0400

    fix: because tobar

--- scenes/protagonist/protagonist.cs
+++ scenes/protagonist/protagonist.cs
@@ -3,7 +3,6 @@ using System;
 
 public partial class Protagonist : Node2D
 {
-	[Export] public int Speed;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,6 +11,5 @@ public partial class Protagonist : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
 	}
 }
